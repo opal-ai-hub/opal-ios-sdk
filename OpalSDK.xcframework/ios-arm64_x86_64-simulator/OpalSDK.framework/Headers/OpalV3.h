c2a1815b0d082d05cb05afbeddc5f1d9c4435ca1
Vasilis Akoinoglou <alladinian@gmail.com>
2025-06-02 10:22:39 +0300

    V4.0.0-b1

---
+++ OpalSDK.xcframework/ios-arm64_x86_64-simulator/OpalSDK.framework/Headers/OpalV3.h
@@ -0,0 +1,19 @@
+//
+//  OpalSDK.h
+//  OpalSDK
+//
+//  Created by Vasilis Akoinoglou on 20/07/2017.
+//  Copyright © 2017 Trebbble. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OpalV3.
+FOUNDATION_EXPORT double OpalV3VersionNumber;
+
+//! Project version string for OpalV3.
+FOUNDATION_EXPORT const unsigned char OpalV3VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpalV3/PublicHeader.h>
+
+
